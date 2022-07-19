e4435d18b6885eb6ba4e8c4d13a9c72135cc0883
masamiti <azain4645@yahoo.co.jpgit push origin sec-02_componentsSlot>
2022-07-19 10:34:56 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+   <v-app>
+    <v-app-bar app>
+      <v-app-bar-title>Zennトレンド一覧（Tech）</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <nuxt-page></nuxt-page>
+    </v-main>
+  </v-app> 
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+  type Article = {
+    title: string
+    linkedCount: number
+    publichedAt: string
+  }
+
+  type State = {
+    articles: Article[]
+  }
+
+  const headers = [
+    { text: '記事名', value: 'title' },
+    { text: 'いいね数', value: 'likedCount' },
+    { text: '公開日時', value: 'publishedAt' },
+  ]
+
+  const state = ref<State>({
+    articles: [],
+  })
+
+  const getZennArticles = async () => {
+    const { data: response } = await useFetch(`https://zenn-api.netlify.app/.netlify/functions/trendTech`)
+    console.log(response.value)
+    
+    state.value.articles = response
+  }
+
+  onMounted(() => {
+    getZennArticles()
+  })
+</script>
+
+<template>
+  test
+  <!-- <v-data-table 
+    :headers="headers"
+    :items="state.value.articles"
+    :items-per-page="5"  
+    class="elevation-1"
+  >
+  </v-data-table> -->
+</template>
